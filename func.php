36e6f0e2281980aab2fc7b502b0dd89014c69418
Hrishik Arora <hrishik.arora@gmail.com>
2020-09-21 13:53:58 +0530

    added authentication

---
+++ func.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+$con=mysqli_connect("localhost","root","","hmsdb");
+if(isset($_POST['login_submit'])){
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+	$query="select * from logintb where username='$username' and password='$password';";
+	$result=mysqli_query($con,$query);
+	if(mysqli_num_rows($result)==1)
+	{
+		$_SESSION['username']=$username;
+		header("Location:admin-panel.php");
+	}
+    else
+    {
+        echo "window.open('index.php');";
+        echo "<script>alert('Enter correct details.');</scripts>";
+    }
+} 
+
+
+
+?>
--- index.php
+++ index.php
@@ -17,12 +17,18 @@
       <button>create</button>
       <p class="message">Already registered? <a href="#">Sign In</a></p>
     </form>
-    <form class="login-form">
-      <input type="text" name="username" placeholder="username"/>
-      <input type="password" name="password" placeholder="password"/>
-      <button namew="login_submit">login</button>
-    
-    </form>
+    <center>
+              <h5>Admin Login</h5><br>
+              <form class="form-group" method="post" action="func.php">
+                <div class="row">
+                  <div class="col-md-4"><label>Username: </label></div>
+                  <div class="col-md-8"><input type="text" name="username" class="form-control" placeholder="enter username" required/></div><br><br>
+                  <div class="col-md-4"><label>Password: </label></div>
+                  <div class="col-md-8"><input type="password" class="form-control" name="password" placeholder="enter password" required/></div><br><br><br>
+                </div>
+                <center><input type="submit" id="inputbtn" name="login_submit" value="Login" class="btn btn-primary"></center>
+              </form>
+            </center>
   </div>
 
 
