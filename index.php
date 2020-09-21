32214895d18732730c0764c576c0ff1a2e118ff7
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2020-09-21 13:40:21 +0530

    login page

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+
+<div class="login-page">
+  <div class="form">
+    <form class="register-form">
+      <input type="text" placeholder="name"/>
+      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="email address"/>
+      <button>create</button>
+      <p class="message">Already registered? <a href="#">Sign In</a></p>
+    </form>
+    <form class="login-form">
+      <input type="text" name="username" placeholder="username"/>
+      <input type="password" name="password" placeholder="password"/>
+      <button namew="login_submit">login</button>
+    
+    </form>
+  </div>
+
+
+
+  <script src="./js/index.js"></script>
+</div>
+
+</body>
+</html>
