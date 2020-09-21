eb1748f4d8ebb5ee9cee6b53b96e3ce0fe150488
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2020-09-21 20:37:00 +0530

    login

--- admin-panel.php
+++ admin-panel.php
@@ -1,11 +1,136 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-   
-</head>
-<body>
- hello
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <style type="text/css"> 
+    button:hover{cursor:pointer;}
+    #inputbtn:hover{cursor:pointer;}
+  </style> 
+    
+      
+  </head>
+  
+  <body style="padding-top:50px;">
+
+  <header>
+        <div class="containerNavbar">
+            <div class="text-area">
+                <a href="index.php">
+                    <span>M</span>
+                    <span>e</span>
+                    <span>d</span>
+                    <span>i</span>
+                    <span>c</span>
+                    <span>a</span>
+                    <span>r</span>
+                    <span>e</span>
+                    <span>+</span>&nbsp;
+                </a>
+
+                
+            </div>
+        </div>
+
+    </header>          
 
 
-</body>
+ <div class="jumbotron" style="color: black" id="ab1"></div>
+   <div class="container-fluid" style="margin-top:50px;">
+    <div class="row">
+  <div class="col-md-4">
+    <div class="list-group" id="list-tab" role="tablist">
+      <a class="list-group-item list-group-item-action active change-now" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Appointment</a>
+      
+      <a class="list-group-item list-group-item-action change-now" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Doctors Section</a>
+      
+      <a class="list-group-item list-group-item-action change-now" id="list-patient-list" data-toggle="list" href="#list-patient" role="tab" aria-controls="settings">Patient list</a>
+    </div><br>
+  </div>
+  <div class="col-md-8">
+    <div class="tab-content" id="nav-tabContent">
+      <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+        <div class="container-fluid">
+          <div class="card">
+            <div class="card-body" id="final">
+              <center><h4>Create an appointment</h4></center><br>
+              <form class="form-group" method="post" action="appointment.php">
+                <div class="row">
+                  <div class="col-md-4"><label>First Name:</label></div>
+                  <div class="col-md-8"><input type="text" class="form-control" name="fname"></div><br><br>
+                  <div class="col-md-4"><label>Last Name:</label></div>
+                  <div class="col-md-8"><input type="text" class="form-control"  name="lname"></div><br><br>
+                  <div class="col-md-4"><label>Email id:</label></div>
+                  <div class="col-md-8"><input type="text"  class="form-control" name="email"></div><br><br>
+                  <div class="col-md-4"><label>Contact Number:</label></div>
+                  <div class="col-md-8"><input type="text" class="form-control"  name="contact"></div><br><br>
+                  <div class="col-md-4"><label>Doctor:</label></div>
+                  <div class="col-md-8">
+                   <select name="doctor" class="form-control" >
+                     <!-- <option value="Dr. Punam Shaw">Dr. Punam Shaw</option>
+                      <option value="Dr. Ashok Goyal">Dr. Ashok Goyal</option> -->
+                      <?php display_docs();?>
+                    </select>
+                  </div><br><br>
+                  <div class="col-md-4"><label>Payment:</label></div>
+                  <div class="col-md-8">
+                    <select name="payment" class="form-control" >
+                      <option value="Paid">Paid</option>
+                      <option value="Pay later">Pay later</option>
+                    </select>
+                  </div><br><br><br>
+                  <div class="col-md-4">
+                    <input type="submit" name="entry_submit" value="Create new entry" class="btn btn-primary" id="inputbtn">
+                  </div>
+                  <div class="col-md-8"></div>                  
+                </div>
+              </form>
+            </div>
+          </div>
+        </div><br>
+      </div>
+      
+      
+      <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+        <div class="card">
+          <div class="card-body">
+            <form class="form-group" method="post" action="func.php">
+              <input type="text" name="contact" class="form-control" placeholder="enter contact"><br>
+              <select name="status" class="form-control">
+                <option value="paid">paid</option>
+                <option value="pay later">pay later</option>
+              </select><br><hr>
+              <input type="submit" value="update" name="update_data" class="btn btn-primary">
+            </form>
+          </div>
+        </div><br><br>
+      </div>
+      <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
+      <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
+        <form class="form-group" method="post" action="func.php">
+          <label>Doctors name: </label>
+          <input type="text" name="name" placeholder="enter doctors name" class="form-control">
+          <br>
+          <input type="submit" name="doc_sub" value="Add Doctor" class="btn btn-primary">
+        </form>
+      </div>
+       <div class="tab-pane fade" id="list-attend" role="tabpanel" aria-labelledby="list-attend-list">...</div>
+    </div>
+  </div>
+</div>
+   </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <!--Sweet alert js-->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.10.1/sweetalert2.all.min.js"></script>
+  </body>
 </html>
